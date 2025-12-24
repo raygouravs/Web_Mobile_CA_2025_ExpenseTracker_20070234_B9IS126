a0f0ca49416b51b00a186f5c987a99906071c9fc
gouravray <gourav.sankar93@gmail.com>
2025-12-24 14:34:22 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Web_Mobile_CA_2025_ExpenseTracker_20070234_B9IS126',
+  webDir: 'dist'
+};
+
+export default config;
