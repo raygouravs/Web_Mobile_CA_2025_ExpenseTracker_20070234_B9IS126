a9a967b88ce3eb6308a4256e787453d236101995
gouravray <gourav.sankar93@gmail.com>
2025-12-26 14:36:43 +0000

    UI development in AddEntry screen

--- src/pages/AddEntry.tsx
+++ src/pages/AddEntry.tsx
@@ -4,7 +4,8 @@
 import {
   IonPage, IonHeader, IonToolbar, IonTitle,
   IonContent, IonInput, IonButton, IonSelect,
-  IonSelectOption, IonLabel, useIonRouter
+  IonSelectOption, IonLabel, IonItem, IonDatetime, 
+  useIonRouter
 } from '@ionic/react';
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
@@ -16,7 +17,12 @@ export default function AddEntry({ history }: any) {
   const [amount, setAmount] = useState<number>(0);
   const [category, setCategory] = useState('');
   const [source, setSource] = useState('');
+  const [date, setDate] = useState(new Date().toISOString());
   const ionRouter = useIonRouter();
+
+  function close() {
+    ionRouter.goBack();
+  }
 
   async function save() {
     if(amount === 0){
@@ -34,7 +40,7 @@ export default function AddEntry({ history }: any) {
     let new_entry = {
       id: `${uuid()}-${new Date().toISOString()}`,
       type,
-      date: new Date().toISOString().split('T')[0],
+      date,
       amount,
       expense_category: category,
       income_source: source
@@ -48,6 +54,16 @@ export default function AddEntry({ history }: any) {
     ionRouter.goBack();
   }
 
+  const handleDateSave = (e: CustomEvent) => {
+  const selectedDate = e.detail.value as string;
+  
+  if (selectedDate) {
+    const dbDate = selectedDate.split('T')[0];
+    setDate(dbDate);
+    console.log("Date saved for DB:", dbDate);
+  }
+};
+
   return (
     <IonPage>
       <IonHeader>
@@ -56,19 +72,65 @@ export default function AddEntry({ history }: any) {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonLabel>Select Entry Type:</IonLabel>
-        <IonSelect value={type} onIonChange={e => setType(e.detail.value)}>
-          <IonSelectOption value="expense">Expense</IonSelectOption>
-          <IonSelectOption value="income">Income</IonSelectOption>
-        </IonSelect>
 
-        <IonLabel>€</IonLabel>
+        <IonItem lines="none" style={{ '--padding-start': '0px' }}>
+          {/*
+          <IonLabel style={{fontSize: '1.0rem', fontWeight: 'bold', color: '#ffc409'}}>Select Entry Type:</IonLabel>
+          <IonSelect value={type} onIonChange={e => setType(e.detail.value)}>
+            <IonSelectOption value="expense">Expense</IonSelectOption>
+            <IonSelectOption value="income">Income</IonSelectOption>
+          </IonSelect>
+          */}
+          <IonSelect 
+            label="Select Entry Type:" 
+            labelPlacement="stacked"
+            fill="solid" 
+            value={type} 
+            onIonChange={e => setType(e.detail.value)}
+            style={{
+              '--background': '#e0e0e0',
+              '--border-radius': '8px',
+              '--color': '#ffc409',
+              '--label-color': '#ffc409',
+              '--highlight-color-focused': '#ffc409',
+              '--highlight-color-valid': '#ffc409',
+              'fontWeight': 'bold',
+              'margin-top': '10px'
+            } as React.CSSProperties}>
+              <IonSelectOption value="expense">Expense</IonSelectOption>
+              <IonSelectOption value="income">Income</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+
+        <div style={{ height: '20px' }} />
+
+        <IonItem lines="none" style={{ '--padding-start': '0px' }}>
+            <IonLabel position="stacked" style={{fontSize: '1.3rem', fontWeight: 'bold', color: '#ffc409'}}>Select Date:</IonLabel>
+            <IonDatetime
+              color='warning'
+              presentation="date"
+              value={date}
+              onIonChange={handleDateSave}
+            />
+        </IonItem>
+
+        <div style={{ height: '8px' }} />
+
+        <IonItem lines="none" style={{ '--padding-start': '0px', '--inner-padding-end': '0px' }}>
+        <IonLabel slot="start" style={{ marginRight: '4px', fontSize: '1.2rem', fontWeight: 'bold', flex: 'none' }}>
+          €
+        </IonLabel>
         <IonInput
           type="number"
-          placeholder="Amount"
+          placeholder="0.00"
           onIonChange={e => setAmount(Number(e.detail.value))}
+          style={{ fontSize: '1.2rem', fontWeight: 'bold',  '--padding-start': '0px' }}
         />
+        </IonItem>
 
+        <div style={{ height: '8px' }} />
+
+        {/*
         {type === 'expense' && (
           <IonInput key="expense_category" placeholder="Category" value={category} onIonInput={e => setCategory(e.detail.value!)} />
         )}
@@ -76,15 +138,78 @@ export default function AddEntry({ history }: any) {
         {type === 'income' && (
           <IonInput key="income_source" placeholder="Source" value={source} onIonInput={e => setSource(e.detail.value!)} />
         )}
-        
-        {/*
-        {type === 'expense' ? 
-        <IonInput placeholder="Category" onIonChange={e => setCategory(e.detail.value!)} /> : 
-        <IonInput placeholder="Source" onIonChange={e => setSource(e.detail.value!)} 
-        />
         */}
 
+        {type === 'expense' && (
+          <>
+            <IonItem lines="none" style={{ '--padding-start': '0px' }}>
+            <IonSelect 
+            key="expense_category"
+            placeholder="Category"
+            label="Expense Category:" 
+            labelPlacement="stacked"
+            fill="solid" 
+            value={category}
+            onIonChange={e => setCategory(e.detail.value!)}
+            style={{
+              '--background': '#e0e0e0',
+              '--border-radius': '8px',
+              '--color': '#ffc409',
+              '--label-color': '#ffc409',
+              '--highlight-color-focused': '#ffc409',
+              '--highlight-color-valid': '#ffc409',
+              'fontWeight': 'bold',
+              'margin-top': '10px'
+            } as React.CSSProperties}>  
+              <IonSelectOption value="bills">Bills & Fees</IonSelectOption>
+              <IonSelectOption value="food">Food & Drink</IonSelectOption>
+              <IonSelectOption value="groceries">Groceries</IonSelectOption>
+              <IonSelectOption value="shopping">Shopping</IonSelectOption>
+              <IonSelectOption value="transport">Transport</IonSelectOption>
+              <IonSelectOption value="entertainment">Entertainment</IonSelectOption>
+              <IonSelectOption value="healthcare">Healthcare</IonSelectOption>
+              <IonSelectOption value="other">Other</IonSelectOption>
+            </IonSelect>
+            </IonItem>
+          </>
+        )}
+
+        <div style={{ height: '8px' }} />
+
+        {type === 'income' && (
+          <>
+            <IonItem lines="none" style={{ '--padding-start': '0px' }}>
+            <IonSelect 
+            key="income_source"
+            placeholder="Source"
+            label="Income Source:" 
+            labelPlacement="stacked"
+            fill="solid" 
+            value={source}
+            onIonChange={e => setSource(e.detail.value!)}
+            style={{
+              '--background': '#e0e0e0',
+              '--border-radius': '8px',
+              '--color': '#ffc409',
+              '--label-color': '#ffc409',
+              '--highlight-color-focused': '#ffc409',
+              '--highlight-color-valid': '#ffc409',
+              'fontWeight': 'bold',
+              'margin-top': '10px'
+            } as React.CSSProperties}>
+              <IonSelectOption value="salary">Salary</IonSelectOption>
+              <IonSelectOption value="business">Business</IonSelectOption>
+              <IonSelectOption value="other">Other</IonSelectOption>
+            </IonSelect>
+            </IonItem>
+          </>
+        )}
+
+        <div style={{ height: '8px' }} />
+    
         <IonButton expand="block" onClick={save} color='warning'>Save</IonButton>
+        <div style={{ height: '8px' }} />
+        <IonButton expand="block" onClick={close} color='danger'>Close</IonButton>
       </IonContent>
     </IonPage>
   );
