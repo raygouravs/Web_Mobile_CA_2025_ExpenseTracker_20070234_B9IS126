d580eb36ffe2f80fb7e5db052de6cb523794d1bf
gouravray <gourav.sankar93@gmail.com>
2025-12-29 09:26:37 +0000

    Main Activity reveretd

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,31 +1,8 @@
-/*
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
-
-}
-*/
-
-package io.ionic.starter;
-
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatDelegate;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        // Force LIGHT mode globally
-        AppCompatDelegate.setDefaultNightMode(
-                AppCompatDelegate.MODE_NIGHT_NO
-        );
-
-        super.onCreate(savedInstanceState);
-    }
+    
 }
